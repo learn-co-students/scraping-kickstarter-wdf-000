b45b6a72fc5e6e57d561b88fecf368aadab4a8ea
satub <satu.barnhill@gmail.com>
2016-07-20 14:02:06 -0400

    Done

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -17,13 +17,14 @@ def create_project_hash
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {
-      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
-      :description => project.css("p.bbcard_blurb").text,
-      :location => project.css("ul.project-meta span.location-name").text,
-      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+      image_link: project.css("div.project-thumbnail a img").attribute("src").value,
+      description: project.css("p.bbcard_blurb").text,
+      location: project.css("ul.project-meta span.location-name").text,
+  # ugly version    location: project.css("ul.project-meta li a").attribute("data-location").value.scan(/short_name.{3}(\w+\,\s\w+)/).first[0]
+      percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
   end
-  #binding.pry
+#  binding.pry
   projects
 end
 
